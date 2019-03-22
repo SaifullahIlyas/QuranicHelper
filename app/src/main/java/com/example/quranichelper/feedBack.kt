280426c5767a11ef891ef49c754e6388e4cd1272
profyp <48782315+profyp@users.noreply.github.com>
2019-03-22 12:12:43 +0500

    implementing Feedback part

--- app/src/main/java/com/example/quranichelper/feedBack.kt
+++ app/src/main/java/com/example/quranichelper/feedBack.kt
@@ -1,8 +1,14 @@
 package com.example.quranichelper
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import java.time.LocalDateTime
 
 class feedBack : AppCompatActivity() {
 
@@ -11,8 +17,36 @@ class feedBack : AppCompatActivity() {
         setContentView(R.layout.activity_feed_back)
         setSupportActionBar(findViewById(R.id.my_toolbar))
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     fun messageSentListner(view:View)
     {
+           message()
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun message() {
+        val input = findViewById<EditText>(R.id.messageFeed)
+        val refrence = FirebaseFirestore.getInstance()
+        val date = LocalDateTime.now()
+        val map = HashMap<String,Any>()
+        map["message"] = input.text.toString()
+        map["time"] = date.toString()
 
+
+        if (input.text != null){
+            try {
+                refrence.collection("Feedback").document("1").set(map).addOnSuccessListener {
+
+                }.addOnFailureListener{e->
+                    Toast.makeText(this,e.message,Toast.LENGTH_LONG).show()
+                }
+            }
+            catch (e:Exception)
+            {
+                Toast.makeText(this,e.message,Toast.LENGTH_LONG).show()
+            }
+        }
+        else{
+            
+        }
     }
 }
