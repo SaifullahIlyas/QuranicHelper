2d97f2717215b10adec4f0e1cc7031c9d00e0116
profyp <48782315+profyp@users.noreply.github.com>
2019-03-23 17:21:33 +0500

    Home Menu through Speech Recognition

--- app/src/main/java/com/example/quranichelper/Home.java
+++ app/src/main/java/com/example/quranichelper/Home.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -24,30 +25,33 @@ import java.util.Locale;
 public class Home extends AppCompatActivity {
     Animation animation;
     ImageView mic,speeker;
+    Intent intent;
     TextToSpeech tts;
     SpeechRecognizer mSP;
     String spkrOutput;
     String menuSpkr;
+    Intent NavigationINtent;
+    Boolean flag=false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
         menuSpkr += "Plese Select one option from the Menu. Menu is ";
-        menuSpkr += "1.Listen  2.Favorite list   3.Downloads   4.Last LIsten  5.FeedBack  6.Help";
+        menuSpkr += "1. Listen  2. Favorite list   3. Downloads   4.Last LIsten  5. FeedBack  6.Help";
           animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
           speeker = findViewById(R.id.speaker);
           speeker.startAnimation(animation);
           intilaizeEngine();
-          maintainSPeakerSpeed(1.0,0.5);
-          getSpeachRecognizer();
-          setInetent();
+
     }
 
 protected void MenuOPtion(View view)
 {
-    Intent intent = new Intent( this,feedbackJ.class);
-    startActivity(intent);
+    //Intent intent = new Intent( this,feedbackJ.class);
+   // startActivity(intent);
+    setInetent();
+    mSP.startListening(intent);
 }
 public void perepereMenu()
 {
@@ -98,8 +102,8 @@ public void perepereMenu()
 
                 @Override
                 public void onBeginningOfSpeech() {
-
-                }
+            Toast.makeText(Home.this,"Speach start",Toast.LENGTH_LONG).show();
+            }
 
                 @Override
                 public void onRmsChanged(float rmsdB) {
@@ -113,11 +117,23 @@ public void perepereMenu()
 
                 @Override
                 public void onEndOfSpeech() {
+                   // Toast.makeText(Home.this,"This is End",Toast.LENGTH_LONG).show();
 
                 }
 
+                @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                 @Override
                 public void onError(int error) {
+                    //Toast.makeText(Home.this,"Error Occur",Toast.LENGTH_LONG).show();
+                    if (error==6)
+                    speeakVoice("please Select the Right option");
+                    try {
+                        mSP.startListening(intent);
+                    }
+                  catch (Exception e)
+                  {
+                      Toast.makeText(Home.this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                  }
 
                 }
 
@@ -125,23 +141,51 @@ public void perepereMenu()
                 @Override
                 public void onResults(Bundle results) {
                     ArrayList<String> result = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                    if(result!=null)
-                        processResult(result.get(0).toString());
+                    if(result!=null) {
+                        String result1 =result.get(0).toString();
+                        String result2 = result.get(1).toString();
+                        processResult(result1,result2);
+                    }
                     else
                         speeakVoice("result is null");
+
+                    mSP.startListening(intent);
                 }
 
                 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-                private void processResult(String mytring)//code to handle commands
+                private void processResult(String mytringRes1,String mytringRes2)//code to handle commands
                 {
+                    String myString;
+                    if(mytringRes1.length()>10 && mytringRes1.contains(""))
+                    {
+                        myString = mytringRes2;
+                    }
+                    else {
+                        myString = mytringRes1;
+                    }
+                    try {
+                        {
+                            if ((myString.contains("five")|| myString.contains("5"))&&myString.length()<10) {
+                                NavigationINtent = new Intent(Home.this,feedbackJ.class) ;
+                                startActivity(NavigationINtent);
+                                flag= true;
+                            }
 
-                    speeakVoice(mytring);
-
+                        }
+                    }
+                   catch (Exception e)
+                   {
+                       Toast.makeText(Home.this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                   }
                 }
 
+
+
+                @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                 @Override
                 public void onPartialResults(Bundle partialResults) {
-
+                    /*ArrayList<String> list = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                      processResult(list.get(0).toString(),list.get(1).toString());*/
                 }
 
                 @Override
@@ -160,11 +204,24 @@ public void perepereMenu()
 
     public void setInetent()//set intent
     {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+         intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_RESULTS,1);
-        mSP.startListening(intent);
+        intent.putExtra(RecognizerIntent.EXTRA_RESULTS,2);
 
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        intilaizeEngine();
+        maintainSPeakerSpeed(1.0,0.5);
+        getSpeachRecognizer();
+        setInetent();
+        flag = false;
+        mSP.startListening(intent);
     }
 
     @Override
@@ -172,6 +229,8 @@ public void perepereMenu()
         super.onPause();
         tts.stop();
         tts.shutdown();
+        mSP.stopListening();
+
     }
 
     @Override
@@ -179,11 +238,18 @@ public void perepereMenu()
         super.onDestroy();
         tts.stop();
         tts.shutdown();
+        mSP.stopListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSP.stopListening();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        intilaizeEngine();
+        mSP.startListening(intent);
     }
 }
---
+++ app/src/main/java/com/example/quranichelper/Listen.java
@@ -0,0 +1,13 @@
+package com.example.quranichelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Listen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listen);
+    }
+}
